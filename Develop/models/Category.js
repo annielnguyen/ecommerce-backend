b5b602a7693abf7d03e61dabdcd2a2b703983d07
Annie Nguyen <anguyen.aln@gmail.com>
2022-01-04 16:56:51 -0800

    added fields and columns for category moel

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -2,18 +2,28 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
+//create our location model
 class Category extends Model {}
-
+//create fields/columns for location model
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    location_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: 'category'
   }
 );
 
--- Develop/server.js
+++ Develop/server.js
@@ -6,9 +6,11 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//add middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//turn on routes
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
